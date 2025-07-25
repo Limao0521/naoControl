50ce5a3a9f52e04fc6fa56e7620b99c2171e9d8d
“andresazcona” <andresazconagla@gmail.com>
2025-07-25 17:40:37 -0500

    commented react mains

--- NaoControlReact/src/App.js
+++ NaoControlReact/src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import NaoController from './components/NaoController';
 import './App.css';
 
+/**
+ * Componente principal de la aplicación que renderiza el NaoController.
+ * Este componente sirve como la raíz de la aplicación React.
+ * @returns {JSX.Element} El componente App renderizado que contiene el NaoController
+ */
 function App() {
   return (
     <div className="App">
