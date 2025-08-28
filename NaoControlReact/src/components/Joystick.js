71f8b5b92dbfd6bff876dc442a02a0d9f999a5f8
“andresazcona” <andresazconagla@gmail.com>
2025-08-28 10:21:09 -0500

    Enhance joystick and control button functionality with responsive design and rotation commands

--- NaoControlReact/src/components/Joystick.js
+++ NaoControlReact/src/components/Joystick.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import useJoystick from '../hooks/useJoystick';
 import './Joystick.css';
 
-const Joystick = ({ onMove, mode }) => {
+const Joystick = ({ onMove, mode, onTurnLeft, onTurnRight }) => {
   const {
     baseRef,
     knobRef,
@@ -10,8 +11,29 @@ const Joystick = ({ onMove, mode }) => {
     handleTouchStart
   } = useJoystick(onMove, mode);
 
+  const handleTurnLeft = () => {
+    if (onTurnLeft) {
+      onTurnLeft();
+    }
+  };
+
+  const handleTurnRight = () => {
+    if (onTurnRight) {
+      onTurnRight();
+    }
+  };
+
   return (
     <div className="joy-wrapper">
+      {/* Botón de rotación izquierda */}
+      <button 
+        className="turn-btn turn-left"
+        onClick={handleTurnLeft}
+        title="Rotar Izquierda"
+      >
+        ←
+      </button>
+
       <div 
         className="joy-base"
         ref={baseRef}
@@ -20,8 +42,24 @@ const Joystick = ({ onMove, mode }) => {
       >
         <div className="joy-knob" ref={knobRef}></div>
       </div>
+
+      {/* Botón de rotación derecha */}
+      <button 
+        className="turn-btn turn-right"
+        onClick={handleTurnRight}
+        title="Rotar Derecha"
+      >
+        →
+      </button>
     </div>
   );
+};
+
+Joystick.propTypes = {
+  onMove: PropTypes.func.isRequired,
+  mode: PropTypes.string.isRequired,
+  onTurnLeft: PropTypes.func,
+  onTurnRight: PropTypes.func
 };
 
 export default Joystick;
